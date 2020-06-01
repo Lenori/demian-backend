c42ac3fd13c00e404884b29cdebe6909838d6fc1
Leo Lenori <leolenori@gmail.com>
2020-06-01 08:15:33 -0300

    remove cors options

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class App {
     }
 
     middlewares() {
-        this.server.use(cors(corsOptions));
+        this.server.use(cors());
         this.server.use(express.json());
         this.server.use(express.static('public'));
     }
