210f10b29eebe3aa4f05771584ad90fedfaa3d97
Leo Lenori <leolenori@gmail.com>
2020-12-21 19:30:58 -0300

    changes to allow development

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,6 +1,10 @@
 export default async (req, res, next) => {
     const whiteList = ['https://escolademianmaia.com.br'];
 
+    if (!req.header('Origin') && process.env.NODE_ENV === 'development') {
+        return next();
+    }
+
     if (!req.header('Origin') || req.header('Origin') && whiteList.indexOf(req.header('Origin')) === -1) {
         return res
             .status(401)
