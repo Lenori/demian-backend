405f2e2e8c6ef16ddca1248a3ce7eff0fc9fc5c0
Leo Lenori <leolenori@gmail.com>
2020-12-21 19:27:23 -0300

    last attempt at cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,10 +1,10 @@
 export default async (req, res, next) => {
     const whiteList = ['https://escolademianmaia.com.br'];
 
-    if (req.header('Origin') && whiteList.indexOf(req.header('Origin')) === -1) {
+    if (!req.header('Origin') || req.header('Origin') && whiteList.indexOf(req.header('Origin')) === -1) {
         return res
             .status(401)
-            .json({error: 'Origin not allowed.'});
+            .json({error: 'Request blocked.'});
     }
 
     return next();
