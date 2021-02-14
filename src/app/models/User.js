338a7a8bec9fc0977e9db5d2633c955a57ee0e4c
Leo Lenori <leolenori@gmail.com>
2021-02-14 16:19:09 -0300

    add phone column

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -14,6 +14,7 @@ class User extends Model {
             cidade: Sequelize.STRING,
             estado: Sequelize.STRING,
             email: Sequelize.STRING,
+            phone: Sequelize.STRING,
             wirecard_id: Sequelize.STRING
         },
         {
