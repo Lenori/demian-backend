53b8057189568cf3ac980d95e17aa3accd1efaba
Leo Lenori <leolenori@gmail.com>
2021-03-23 12:59:29 -0300

    change whitelist cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,13 @@
 export default async (req, res, next) => {
-    const whiteList = ['https://escolademianmaia.com.br', 'http://localhost:3000'];
+    const whiteList = [
+        'https://escolademianmaia.com.br',
+        'http://localhost:3000',
+        'https://dmescolajj.com.br',
+        'https://dmescolajjb.com.br',
+        'https://escolademianmaiajj.com.br',
+        'https://escoladmjj.com.br',
+        'https://escoladmjjb.com.br'
+    ];
 
     if (!req.header('Origin') && process.env.NODE_ENV === 'development') {
         return next();
